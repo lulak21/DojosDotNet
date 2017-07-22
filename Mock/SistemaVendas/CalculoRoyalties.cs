c6dc8a6acd3575734eb4e02860abf237526e83f0
Luiz Rodrigues <luizphr@gmail.com>
2017-07-22 08:44:17 -0300

    Inclusao do Dojo de Mock usando Moq e MSTests

---
+++ Mock/SistemaVendas/CalculoRoyalties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaVendas
+{
+    public class CalculoRoyalties
+    {
+        private SistemaVendas.IRepositorioVendas iRepositorioVendas;
+        private CalculoComissao calculoComissao ;
+        public CalculoRoyalties(SistemaVendas.IRepositorioVendas iRepositorioVendas, CalculoComissao calculoComissao)
+        {
+            this.calculoComissao = calculoComissao;
+            this.iRepositorioVendas = iRepositorioVendas;
+        }
+        public Double CalculaRoyalties(int mes, int ano)
+        {
+            Double royalty = 0.0;
+            
+            List<double> vendas = iRepositorioVendas.GetVendas(mes, ano);
+            
+            foreach (var venda in vendas)
+            { 
+                var valorLiquido = venda - calculoComissao.calculaComissao(venda);
+                royalty += valorLiquido * 0.2;
+            }
+            
+            return royalty;
+        }
+    }
+}
