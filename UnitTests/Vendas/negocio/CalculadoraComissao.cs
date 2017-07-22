4af9e4f6492a712a75ea87885c601d347b5e1651
Luiz Rodrigues <luizphr@gmail.com>
2017-07-22 08:26:40 -0300

    Commmit inicial com inclusao do Dojo de Unit Tests

---
+++ UnitTests/Vendas/negocio/CalculadoraComissao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vendas.negocio
+{
+    public class CalculadoraComissao
+    {
+        public double calculaComissao(double totalVendas)
+        {
+            return Math.Floor(5.0 * totalVendas) / 100;
+        }
+    }
+}
